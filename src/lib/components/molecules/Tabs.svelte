bad7a73d2287202311369963adfb87cb86b34368
Shrey Kumar <shreykr2802@gmail.com>
2025-01-03 12:13:35 +0530

    Tabs Updated

--- src/lib/components/molecules/Tabs.svelte
+++ src/lib/components/molecules/Tabs.svelte
@@ -1,35 +1,56 @@
 <script lang="ts">
 	let tabs = [
 		{
-			title: 'section 1',
-			content: 'content 1'
+			title: 'section 1'
 		},
 		{
-			title: 'section 2',
-			content: 'content 2'
+			title: 'section 2'
 		},
 		{
-			title: 'section 3',
-			content: 'content 3'
+			title: 'section 3'
 		}
 	]
 	let { activeTab, handleTabClick } = $props()
 </script>
 
-<div>
-	<div class="flex border-b border-gray-200">
+<div class="tabs-container">
+	<div class="tabs">
 		{#each tabs as tab, index}
-			<button
-				class=" {activeTab === index ? 'text-blue-500 border-b-2 border-blue-500' : ''}"
-				onclick={() => handleTabClick(index)}
-			>
+			<button class={activeTab === index ? 'active' : ''} onclick={() => handleTabClick(index)}>
 				{tab.title}
 			</button>
 		{/each}
 	</div>
-	<div class="p-6">
-		{#if tabs[activeTab]}
-			{tabs[activeTab].content}
-		{/if}
-	</div>
 </div>
+
+<style lang="scss">
+	.tabs-container {
+		width: 50%;
+		padding: 1em;
+		margin: 1em;
+		.tabs {
+			display: flex;
+			flex-flow: row;
+			justify-content: space-between;
+			align-items: center;
+			height: 4em;
+			gap: 1em;
+
+			button {
+				width: 33%;
+				height: 100%;
+				outline: 0;
+				border: 0;
+				background-color: var(--color-white-900);
+				cursor: pointer;
+				transition: all 01s ease;
+				border-radius: 1em;
+				box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+
+				&.active {
+					background-color: var(--color-cyan-700);
+				}
+			}
+		}
+	}
+</style>

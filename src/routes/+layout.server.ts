9735efcf267cae62ef90200d4f0dd410ea2692b6
Shrey Kumar <shreykr2802@gmail.com>
2025-02-27 21:10:13 +0530

    Inital Test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,13 @@
 import type { LayoutServerLoad } from './$types'
 import { PUBLIC_APPWRITE_DATABASE, PUBLIC_APPWRITE_HOME_PAGE_INFO_DB } from '$env/static/public';
 
-export const load: LayoutServerLoad = async ({ locals: { user, databases }, cookies }) => {
+export const load: LayoutServerLoad = async ({ locals: { user, supabase }, cookies }) => {
   let top_banner = "Launching Soon with exciting offers."
   try {
-    const home_pageInfo = await databases?.listDocuments(PUBLIC_APPWRITE_DATABASE, PUBLIC_APPWRITE_HOME_PAGE_INFO_DB);
-    top_banner = home_pageInfo.documents[0].top_banner ?? null;
+    const home_pageInfo = await supabase.from('home_page')
+      .select('*')
+    console.log('home_pageInfo', home_pageInfo)
+    top_banner = home_pageInfo.data[0].top_banner
   } catch (error) {
     console.error('Error fetching home page info:', error);
   }

3a98e7124c2e7823abb2637719714060361706aa
Shrey Kumar <shreykr2802@gmail.com>
2025-04-01 09:12:44 +0530

    plan change

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,11 +3,15 @@ import type { LayoutServerLoad } from './$types'
 export const load: LayoutServerLoad = async ({ locals: { safeGetSession, supabase }, cookies }) => {
   const { session, user } = await safeGetSession()
   let top_banner = "Launching Soon with exciting offers."
+  let plans = {};
   try {
-    const { data } = await supabase.from('home_page')
+    const { data, error } = await supabase.from('home_page')
       .select('*')
+    console.log("data", data, error)
     if (data) {
       top_banner = data[0]?.top_banner
+      plans = data[0]?.plans
+
     }
   } catch (error) {
     console.error('Error fetching home page info:', error);
